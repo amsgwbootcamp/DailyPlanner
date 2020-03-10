496c60014232e98a101105399fa4504c969de3b2
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-03-10 11:19:38 -0400

    Partial working textarea fields

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+var nineInfo = "";
+var tenInfo = "";
+var elevenInfo = "";
+var twelveInfo = "";
+var oneInfo = "";
+var twoInfo = "";
+var threeInfo = "";
+var fourInfo = "";
+var fiveInfo = "";
+
 $(".saveEvent").on("click", function() {
     buttonPressed = $(this).val();
-    alert("You pressed the " + buttonPressed + " Save Button.")
-    /*$("#operator").text($(this).text());  */
-
+    alert("You pressed the " + buttonPressed + " Save Button.");
+    var timeSlot = $(".nineInfo");
+    alert(timeSlot.val());
+    
   });
 
   $(document).ready(function(){
       var currentDayEl = $("p.currentDay");
-      currentDayEl.text(moment().format('MMMM Do YYYY'));
+      var currDate = moment().format('MMMM Do YYYY');
+      var currDay = moment().format('dddd');
+      currentDayEl.text(currDay + " " + currDate);
    });
